acca1dbd71c9873c127aeb437aefb17d614fd8c8
daeunLee <eleeje97@gmail.com>
2020-06-12 02:51:13 +0900

    베스트 앨범

--- Algorithm/src/Programmers/Main.java
+++ Algorithm/src/Programmers/Main.java
@@ -1,10 +1,22 @@
 package Programmers;
 
+import Programmers.hash.BestAlbum;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		BestAlbum solution = new BestAlbum();
+		String[] genres	= {"classic", "pop", "classic", "classic", "pop"}; //"classic", "classic", "classic", "pop"
+		int[] plays = {500, 600, 150, 800, 2500}; //500, 600, 150, 800, 2500
+		//answer: [4, 1, 3, 0]
+		
+		int[] answer = solution.solution(genres, plays);
+		for (int i : answer) {
+			System.out.print(i + " ");
+		}
+		
 	}
 
 }
