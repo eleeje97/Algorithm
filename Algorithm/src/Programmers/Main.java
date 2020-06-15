17702d5ba793b28eceb71d96c5b9024bac4b6a83
daeunLee <eleeje97@gmail.com>
2020-06-15 23:28:02 +0900

    스택/큐 - 탑

--- Algorithm/src/Programmers/Main.java
+++ Algorithm/src/Programmers/Main.java
@@ -1,18 +1,17 @@
 package Programmers;
 
-import Programmers.hash.BestAlbum;
+import Programmers.stack_queue.*;;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		BestAlbum solution = new BestAlbum();
-		String[] genres	= {"classic", "pop", "classic", "classic", "pop"}; //"classic", "classic", "classic", "pop"
-		int[] plays = {500, 600, 150, 800, 2500}; //500, 600, 150, 800, 2500
-		//answer: [4, 1, 3, 0]
-		
-		int[] answer = solution.solution(genres, plays);
+		Tower solution = new Tower();
+		//int[] heights = {6,9,5,7,4};	//[0,0,2,2,4]
+		//int[] heights = {3,9,9,3,5,7,2};	//[0,0,0,3,3,3,6]
+		int[] heights = {1,5,3,6,7,6,5};	//[0,0,2,0,0,5,6]
+		int[] answer = solution.solution(heights);
 		for (int i : answer) {
 			System.out.print(i + " ");
 		}
