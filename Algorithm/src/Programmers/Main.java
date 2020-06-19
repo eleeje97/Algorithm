4bb556c66dd48820250d39c588086d0ba3f3ee18
daeunLee <eleeje97@gmail.com>
2020-06-19 20:11:06 +0900

    힙 - 라면공장

--- Algorithm/src/Programmers/Main.java
+++ Algorithm/src/Programmers/Main.java
@@ -1,27 +1,39 @@
 package Programmers;
 
-import Programmers.stack_queue.*;;
+import Programmers.heap.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Printer solution = new Printer();
+		RamenFactory solution = new RamenFactory();
+
+//		int stock = 4;
+//		int[] dates = {4,10,15};
+//		int[] supplies = {20,5,10};
+//		int k = 30;
+//		// return: 2
+
+//		int stock = 4;
+//		int[] dates = {4,10,18};
+//		int[] supplies = {12,8,12};
+//		int k = 30;
+//		// return: 3
 		
-		//int[] priorities = {2,1,3,2};
-		//int location = 2;
-		// return: 1
+//		int stock = 4;
+//		int[] dates = {1,2,3,4};
+//		int[] supplies = {1,1,1,1};
+//		int k = 6;
+//		// return: 2
+
+		int stock = 4;
+		int[] dates = {2,4,10,15};
+		int[] supplies = {15,20,5,3};
+		int k = 30;
+		// return: 2
 		
-		//int[] priorities = {1,1,9,1,1,1};
-		//int location = 0;
-		// return: 5
-		
-		int[] priorities = {1,8,2,3,7,2,2};
-		int location = 6;
-		// return: 5
-		
-		int answer = solution.solution(priorities, location);
+		int answer = solution.solution(stock, dates, supplies, k);
 		System.out.println(answer);
 		
 	}
