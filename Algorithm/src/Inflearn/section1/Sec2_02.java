da4703f5ae39b4b381d24791053e62a97cbf4419
daeunLee <eleeje97@gmail.com>
2022-02-16 01:01:44 +0900

    Section2 Array 1~3

---
+++ Algorithm/src/Inflearn/section1/Sec2_02.java
@@ -0,0 +1,30 @@
+package Inflearn.section1;
+
+import java.util.Scanner;
+
+public class Sec2_02 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int[] heights = new int[num];
+        for (int i = 0; i < num; i++) {
+            heights[i] = scn.nextInt();
+        }
+
+        System.out.println(solution(heights));
+    }
+
+    public static int solution(int[] heights) {
+        int answer = 0;
+        int current = 0;
+
+        for (int height : heights) {
+            if (height > current) {
+                answer++;
+                current = height;
+            }
+        }
+
+        return answer;
+    }
+}
