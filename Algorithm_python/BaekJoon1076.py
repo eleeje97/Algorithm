f07fbdc24256eff59bf099290d0e1d87d3c78b65
daeunLee <eleeje97@gmail.com>
2022-03-03 22:30:05 +0900

    BaekJoon 1076,1085,1100

---
+++ Algorithm_python/BaekJoon1076.py
@@ -0,0 +1,5 @@
+registors = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4, "green": 5, "blue": 6, "violet": 7, "grey": 8,
+             "white": 9}
+
+value = ((registors[input()] * 10) + registors[input()]) * (10 ** registors[input()])
+print(value)
---
+++ Algorithm_python/BaekJoon1100.py
@@ -0,0 +1,14 @@
+count = 0
+
+for i in range(8):
+    line = input()
+    if i % 2 == 0:
+        for j in range(0, 8, 2):
+            if line[j] == "F":
+                count += 1
+    else:
+        for j in range(1, 8, 2):
+            if line[j] == "F":
+                count += 1
+
+print(count)
