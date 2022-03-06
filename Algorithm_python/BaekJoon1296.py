e3c1cbdb51c0a51cedba4ed14dbebf0bc1121aee
daeunLee <eleeje97@gmail.com>
2022-03-06 22:26:36 +0900

    BaekJoon 1296

--- Algorithm_python/BaekJoon1296.py
+++ Algorithm_python/BaekJoon1296.py
@@ -1,21 +1,13 @@
 import itertools
 
+love = "LOVE"
 name = input()
 alpha_dict = {}
-for i in name:
-    alpha_dict[i] = alpha_dict.get(i, 0) + 1
-
-
-def getScore(teamName):
-    for j in teamName:
-        if alpha_dict.keys().__contains__(j):
-            alpha_dict[j] += 1
-
-    score = 1
-    for j in itertools.combinations(alpha_dict.values(), 2):
-        score *= sum(list(j))
-    return score % 100
-
+for k in love:
+    alpha_dict.setdefault(k, 0)
+for k in name:
+    if alpha_dict.keys().__contains__(k):
+        alpha_dict[k] += 1
 
 num = int(input())
 lst = []
@@ -24,12 +16,26 @@ for i in range(num):
 
 lst.sort()
 
+
+def getScore(teamName):
+    temp = alpha_dict.copy()
+    for j in teamName:
+        if temp.keys().__contains__(j):
+            temp[j] += 1
+
+    score = 1
+    for j in itertools.combinations(temp.values(), 2):
+        score *= sum(list(j))
+    return score % 100
+
+
 max = 0
+answer = lst[0]
 for i in lst:
     score = getScore(i)
-    print(score)
     if score > max:
         max = score
         answer = i
+
 
 print(answer)
