41c37cfc44a2e31b708bd98fa9c711dd83dbe56f
danalee <eleeje97@gmail.com>
2022-10-14 13:56:06 +0900

    [Programmers] 게임 맵 최단거리

---
+++ Algorithm_python/programmers/dfsbfs/gameMap.py
@@ -0,0 +1,74 @@
+from collections import deque
+global dx, dy, visited
+
+
+# 상하좌우 방향 배열
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+
+def solution(maps):
+    global visited
+    visited = [[0 for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    return bfs(maps)
+
+
+''' 시간초과
+def bfs(maps):
+    global dx, dy, visited
+    queue = deque()
+    queue.append([0, 0, 1])
+
+    while queue:
+        p = queue.popleft()
+        row = p[0]
+        col = p[1]
+        depth = p[2]
+
+        print(row, col, depth)
+        if row == len(maps) - 1 and col == len(maps[0]) - 1:
+            # queue.clear()
+            return depth
+
+        visited[row][col] = 1
+        for i in range(4):
+            nr = row + dy[i]
+            nc = col + dx[i]
+
+            if 0 <= nr < len(maps) and 0 <= nc < len(maps[0]) and visited[nr][nc] == 0 and maps[nr][nc] == 1:
+                queue.append([nr, nc, depth+1])
+                # visited[nr][nc] = 0
+
+    return -1
+
+'''
+
+
+def bfs(maps):
+    global dx, dy, visited
+    queue = deque()
+    queue.append([0, 0])
+
+    while queue:
+        p = queue.popleft()
+        row = p[0]
+        col = p[1]
+
+        print(row, col, maps[row][col])
+        if row == len(maps) - 1 and col == len(maps[0]) - 1:
+            return maps[row][col]
+
+        visited[row][col] = 1
+        for i in range(4):
+            nr = row + dy[i]
+            nc = col + dx[i]
+
+            if 0 <= nr < len(maps) and 0 <= nc < len(maps[0]) and visited[nr][nc] == 0 and maps[nr][nc] == 1:
+                queue.append([nr, nc])
+                maps[nr][nc] = maps[row][col] + 1
+
+    return -1
+
+
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
